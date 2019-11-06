d2575c3bdf299932f0553ef48d133cf56e911c34
musabfiqi <musab.fiqi99@gmail.com>
2019-11-06 18:35:34 -0500

    Commit

--- src/edu/cscc/Node.java
+++ src/edu/cscc/Node.java
@@ -4,23 +4,26 @@ public class Node {
     private Object content;
     private Node next;
 
+    /**
+     * Constructor
+     * @param content
+     * @param next
+     */
     public Node(Object content, Node next) {
         this.content = content;
         this.next = next;
     }
 
+    //Setters and Getters
     public Object getContent() {
         return content;
     }
-
     public void setContent(Object content) {
         this.content = content;
     }
-
     public Node getNext() {
         return next;
     }
-
     public void setNext(Node next) {
         this.next = next;
     }
